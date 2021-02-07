29a538d868a1585aa2d2ff6e9f5271c2233c0f7c
Steven Elliott <steven.elliott@live.com>
2021-02-06 21:45:03 -0500

    added update to ajax pull to include screenshot url

--- js/script.js
+++ js/script.js
@@ -12,6 +12,9 @@
       
             console.log(elem.name);
             $("#result-content").append("<p>" + elem.name + "</p>");
+
+            console.log(elem.short_screenshots[0].image);
+            $("#result-img").append("<a href=" + elem.short_screenshots[0].image + "style='width:100px;height:200px;'</a>");
           })
       
         }
